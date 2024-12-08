082266f89b06875525224f6fba01427d0405f435
Saiteja <53438101+svpsaiteja@users.noreply.github.com>
2024-12-08 21:11:54 +0530

    Routing error handling & Private routes

--- src/routing/HomePage.tsx
+++ src/routing/HomePage.tsx
@@ -4,10 +4,10 @@ const HomePage = () => {
   return (
     <>
       <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-        Incidunt, mollitia!
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt,
+        mollitia!
       </p>
-      <Link to='/users'>Users</Link>
+      <Link to="/users">Users</Link>
     </>
   );
 };
--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -4,20 +4,29 @@ import HomePage from "./HomePage";
 import Layout from "./Layout";
 import UserDetail from "./UserDetail";
 import UsersPage from "./UsersPage";
+import ErrorPage from "./ErrorPage";
+import PrivateRoutes from "./PrivateRoutes";
+import LoginPage from "./LoginPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "", element: <HomePage /> },
-      {
-        path: "users",
-        element: <UsersPage />,
-        children: [{ path: ":id", element: <UserDetail /> }],
-      },
-
       { path: "contact", element: <ContactPage /> },
+      { path: "login", element: <LoginPage /> },
+      {
+        element: <PrivateRoutes />,
+        children: [
+          {
+            path: "users",
+            element: <UsersPage />,
+            children: [{ path: ":id", element: <UserDetail /> }],
+          },
+        ],
+      },
     ],
   },
 ]);
